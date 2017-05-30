42de29c05cfae3a8a3903b7ca94557aaa7870802
slauriano <slauriano@MAC-SRG.local>
2017-05-29 22:09:30 -0300

    Project1 - 29-05-2017
    let fm = FileManager.default // load a system file class
    let path = Bundle.main.resourcePath! // load a bundle of path to manage files
    let items = try! fm.contentsOfDirectory(atPath: path) // load the items from the path
    for item in items { // loop the content of path to an array

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var pictures = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let fm = FileManager.default // load a system file class
+        let path = Bundle.main.resourcePath! // load a bundle of path to manage files
+        let items = try! fm.contentsOfDirectory(atPath: path) // load the items from the path
+        
+        for item in items { // loop the content of path to an array
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+            }
+        }
+        print(pictures)
    }
 
     override func didReceiveMemoryWarning() {
